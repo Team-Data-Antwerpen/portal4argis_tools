f26af6b47a2bf9b9f5c810fa8a75a06127a0a51c
Kay Warrie <kaywarrie@gmail.com>
2017-03-21 16:55:08 +0100

    added argparse and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+Metadata to Portal 
+===================
+
+One the problems with the portal for arcgis is that the metadata of a dataset in the geodatabase, isn't uploaded to the  server. So the user needs to manualy copy paste the description of the layer, author, ... from metadata to the portal every time he edits the metadata.
+
+This script will upload created a item in the portal with the correct metadata for every item in a Arcgis mapdocument. The user needs to provide the link to the service. If a item already exists with the same name t will be overwriten. 
+
+So you can run this script daily to update changes in the metadata. 
+
+    usage: metadata2portal.py [-h] [--portal PORTAL] [--user USER]
+                              [--password PASSWORD] [--mxd MXD]
+                              [--service SERVICE]
+
+    optional arguments:
+      -h, --help           show this help message and exit
+      --portal PORTAL      the link to the ESRI argis Portal
+      --user USER          the username of the ESRI argis Portal
+      --password PASSWORD  the password of the ESRI argis Portal
+      --mxd MXD            the mxd with sync with the ESRI argis Portal
+      --service SERVICE    the link to !CORRESPONDING! mapservice of the mxd
--- metadata2portal.py
+++ metadata2portal.py
@@ -9,55 +9,72 @@
 # Created:     20/03/2017
 # Copyright:   (c) Kay Warrie 2017
 # Licence:     MIT
+# usage: metadata2portal.py [-h] [--portal PORTAL] [--user USER] [--mxd MXD] 
+#                                [--password PASSWORD] [--service SERVICE]
+#
+# optional arguments:
+# -h                   show this help message and exit
+# --portal PORTAL      the link to the ESRI argis Portal
+# --user USER          the username of the ESRI argis Portal
+# --password PASSWORD  the password of the ESRI argis Portal
+# --mxd MXD            the mxd with sync with the ESRI argis Portal
+# --service SERVICE    the link to !CORRESPONDING! mapservice of the mxd
 #-------------------------------------------------------------------------------
-import sys, os, arcpy, json
+import argparse, arcpy
 from portal import additem, shareItem, generateToken, getUserContent, updateItem
 from portal.metadata import metadata
 
-PORTAL = "https://devas1179.dev.digant.antwerpen.local/arcgis"
-USER   = "JoostSchouppe"
-PASS   = "schouppe1"
-
-#MXD and !CORRESPONDING! SERVICE
-MXD = r"I:\2_05_06_Publicatie\Geoportaal_projectmap\script\testdata\OpenDataAntwerpen.mxd"
-SERVICE = "http://geodata.antwerpen.be/arcgissql/rest/services/P_Publiek/OpenDataAntwerpen/MapServer/"
-
-def listLayersInMxd(mxdFile, token):
+def listLayersInMxd(mxdFile, token, service, user, portal):
     mxd = arcpy.mapping.MapDocument(mxdFile)
     lyrs = arcpy.mapping.ListLayers( mxd )
     for nr in range( len(lyrs)):
         lyr = lyrs[nr]
         if hasattr(lyr, "dataSource") and arcpy.Exists(lyr.dataSource):
-            addLyr(lyr.dataSource, lyr.name, nr, token )
+            addLyr(lyr.dataSource, lyr.name, nr, token, service, user, portal)
         else:
             print( lyr.name + " has no valid datasource " )
 
-def addLyr(dataSource, name, nr, token):
+def addLyr(dataSource, name, nr, token, service, user, portal):
     meta = metadata.metadataFromArcgis( dataSource )
     author = meta.credits if len( meta.credits ) else "Stad Antwerpen"
 
-    userContent = getUserContent(USER, '', token, PORTAL )
+    userContent = getUserContent(user, '', token, portal )
     existingIDs = { n['title'] : n['id'] for n in userContent["items"]}
 
     if name in existingIDs.keys():
         print "updating " + name
-        item = updateItem(USER, token, PORTAL, existingIDs[name], SERVICE + str(nr),
+        item = updateItem(user, token, portal, existingIDs[name], service + str(nr),
                  title=name, summary=meta.purpose, description=meta.description, author=author)
     else:
         print "adding " + name
-        item = additem( USER, token, PORTAL, SERVICE + str(nr),
+        item = additem( user, token, portal, service + str(nr),
              title=name, summary=meta.purpose, description=meta.description, author=author)
 
     if "success" in item.keys() and item["success"]:
         id = item["id"]
-        print shareItem(id, token, PORTAL, True, True, [] )
+        print shareItem(id, token, portal, True, True, [] )
     else:
         raise Exception( "Error uploading "+ name +" : "+ str(item) )
 
 
 def main():
-    token = generateToken(USER, PASS, PORTAL)
-    listLayersInMxd(MXD, token)
+    #Defaults
+    PORTAL = "https://devas1179.dev.digant.antwerpen.local/arcgis"
+    USER   = "JoostSchouppe"
+    PASS   = "schouppe1"
+    MXD = r"I:\2_05_06_Publicatie\Geoportaal_projectmap\script\testdata\data.mxd"
+    SERVICE = "http://geodata.antwerpen.be/arcgissql/rest/services/P_Publiek/OpenDataAntwerpen/MapServer/"
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--portal",   help="the link to the ESRI argis Portal", default=PORTAL)
+    parser.add_argument("--user",     help="the username of the ESRI argis Portal", default=USER)
+    parser.add_argument("--password", help="the password of the ESRI argis Portal", default=PASS)
+    parser.add_argument("--mxd",      help="the mxd with sync with the ESRI argis Portal", default=MXD)
+    parser.add_argument("--service",  help="the link to !CORRESPONDING! mapservice of the mxd", default=SERVICE)
+    args = parser.parse_args()
+
+    token = generateToken(args.user, args.password, args.portal)
+    listLayersInMxd(args.mxd, token, args.service, args.user, args.portal)
 
 if __name__ == '__main__':
     main()
